fdfa44f2c38b9bb15e84b76129cbb21e8051096e
noaheakin <noaheakin@gmail.com>
2021-01-18 16:04:57 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(stats) {
+  let result = stats.find( stat => stat.result === "W" )
+  return result ? result.year : undefined
+}
